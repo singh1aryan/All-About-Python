4dfe079e3c3662bca40be28d8549416343c8031b
aryan <aryansingh@umass.edu>
2019-03-17 23:12:31 -0400

    array,string permutation

--- data_structures/permutation_string.py
+++ data_structures/permutation_string.py
@@ -5,9 +5,13 @@ Created on Sun Mar 17 21:09:40 2019
 @author: Aryan Singh
 """
 
-# permutations of a string
+'''
+recursive implementation of the string permutations
+First method:
+    1. uses the substring property of strings (not valid for arrays)
+    2. 
 
-# recursive implementation
+'''
 
 
 def permutationString(s, results):
@@ -23,3 +27,30 @@ def permutation(prefix, suffix, results):
             
 l = ['']
 print(permutationString("abc", l))
+
+'''
+2nd implementation, using the swap method - smaller/easier in Python
+    1. string
+    2. starting index of string
+    3. ending index of string
+'''
+
+
+def permutationString2(a,l,r):
+    if l==r:
+        print(a)
+    else:
+        for i in range(l,r+1):
+            swap(a,i,l)
+            permutationString2(a,l+1,r)
+            swap(a,i,l) # backtrack
+            
+def swap(arr, i, j):
+    temp = arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+    
+string = "ABC"
+n = len(string) 
+a = list(string) 
+permutationString2(a,0,n-1)
--- permutation_array.py
+++ permutation_array.py
@@ -12,5 +12,37 @@ Created on Sun Mar 17 21:23:58 2019
 we cannot separate this into [1], [2,3] like the strings
 we should keep it in one array, and have a pointer going through it
 we would swap the elements and call in recursively
+we would have to swap back again to 'not chagnge' anything in the array
+
+we would be swapping the index with itself, but that does not change anything
+it is recursive and that's why we need the base case
+
+Example: [1,2,3]
+[1,2,3] - itself
+[1,3,2] - swaps 2,3 because its a recursive call and goes deep
+[2,1,3] - swaps 1,2 as the first deep recusive calls are finished now
+
+follows similar pattern
 
 '''
+
+def permutation_array(array,results):
+    permutation(array,0,results)
+    return results
+    
+def permutation(a, start, results):
+    if start>=len(a):
+        results.append(a[:])
+    else:
+        for i in range(start,len(a)): # values would duplicate if we have '0' here
+            swap(a,start,i)
+            permutation(a,start+1,results)
+            swap(a,start,i)
+    
+def swap(arr, i, j):
+    temp = arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+    
+res = []
+print(permutation_array([1,2,3], res))

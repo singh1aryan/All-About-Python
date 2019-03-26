af94fa8ca1bf2ff672d1cf3881c82038174fb20e
aryan <aryansingh@umass.edu>
2019-03-26 17:27:15 -0400

    resolver

---
+++ leetcode/278.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 02:32:25 2019
+
+@author: Aryan Singh
+"""
+
+'''
+
+First bad Version
+
+Each version is based on previous versions
+we want to find the first bad version, based on that
+
+Ex:
+    3-false
+    5-true
+    4-true
+    4 is the first bad version
+
+'''

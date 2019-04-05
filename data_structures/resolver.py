ff651828a69a6abdc99540b3c92ed443a68e12f1
aryan <aryansingh@umass.edu>
2019-04-05 15:36:35 -0400

    problems

--- data_structures/resolver.py
+++ data_structures/resolver.py
@@ -1,34 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 26 15:36:00 2019
-
-@author: Aryan Singh
-"""
-
-'''
-Your code should output a single line of text, starting with “true” (the theorem was proved) or
-“false”, followed by a space and a integer indicating the number of times the your code applied
-the resolution rule while attempting to prove the theorem.
-For example:
-false 42
-
-A V -B
-
-we have the alpha and we negate it always
-we then use the kb and then add alpha to it
-we prove by contradiction always
-
-we take c1 and c2, solve that to find c3 and do that recursively
-R
--R V S
-
-empty clause, resolution rule kills it
-'''     
+import itertools
+import sys
 
 kb = []
-alpha = ['-F']
-def resolver(clauses):
-    #clauses = KB.clauses + conjuncts(~alpha)
+alpha = []
+def resolver(clauses, alpha):
+    for i in alpha:
+        clauses.append(i)
     c=0
     new = []
     while True:
@@ -43,19 +20,19 @@ def resolver(clauses):
             # if there is a clause with i and -i, don't add it
             for res in resolvents:
                 if len(res) == 0:
-                    print(c)
+                    print('true ' + str(c))
                     return True
                 if res not in new:
                     new.append(res)
                     c+=1
             
-        # if new ⊆ clauses then return false
+        # if new clauses then return false
         m=0
         for i in new:
             if i in clauses:
                 m+=1
         if m == len(new):
-            print(c)
+            print('false ' + str(c))
             return False
             
         # adding to clauses
@@ -119,43 +96,53 @@ def pl_resolve(ci, cj):
         
     return ll
             
+def negate_alpha(arr):
+    l = []
+    for i in range(0,len(arr)):
+        if not arr[i].isalpha():# not a letter
+           l.append(arr[i].split('-')[1])
+        else:
+            l.append('-'+arr[i])
     
-with open('test2.txt') as f:
+    return l
+# recursive
+def dnf_cnf(a):
+    return list(itertools.product(*a))
+
+def clean(cnf):
+    # lists of list - cnf
+    a = []
+    for i in range(0,len(cnf)):
+        b = list(set(cnf[i]))# delete duplicates for all
+        found = 0
+        for k in b:
+            if '-'+k in b:
+                found = 1    
+        if found == 0:
+            a.append(b)
+        
+    return a
+    
+with open(sys.argv[2]) as f:
     #print(f.read())
     c=0
-    cnf = ""
+    dnf = []
+    cnf = []
+    lines = f.readlines()
+    for i in lines:
+        thisline = i.split()
+        dnf.append(negate_alpha(thisline))
+    
+    dnf = dnf_cnf(dnf)
+    for i in dnf:
+        cnf.append(list(i))
+    alpha = clean(cnf)
+    
+        
+''' Reading the KB file '''
+with open(sys.argv[1]) as f:
     lines = f.readlines();
     for i in lines:
         thisline = i.split();
         kb.append(thisline)
-    cnf = cnf[0:len(cnf)-1]
-    kb.append(alpha)
-    print(resolver(kb)) 
-   
-'''
-a = 'a'
-print(a.split('-'))
-
-[R] and [-R] - [R][]
-
-print(pl_resolve(['R'],['Q','-R'])) # Q
-
-print(pl_resolve(['-P','-R'],['Q','R']))# -P,Q
-
-print(pl_resolve(['-P','R'],['Q','-R']))# -P, R
-
-print(pl_resolve(['-P','Q'],['P','Q']))# Q
-
-print(pl_resolve(['-P','-Q'],['P','Q']))# -Q , Q, do we return P -P here?
-
-print(pl_resolve(['-P'],['P']))# []
-
-print(pl_resolve(['-P','Q','R','S'],['P','Q','R','-S']))# gives 2
-
-#print(pl_resolve(['-P','Q','-R','S'],['P','Q','R','-S']))# gives 3
-
-'''
-mat = [[1,2,3],[4,5,6],[1,2,3],[7,8,9],[4,5,6]]
-set(tuple(row) for row in mat)
-a = [list(item) for item in set(tuple(row) for row in mat)]
-print(a)
+    resolver(kb,alpha)

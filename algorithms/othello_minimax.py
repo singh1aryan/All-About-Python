92ff105f8be9485b9171774851c37b16c66a1b44
aryan <aryansingh@umass.edu>
2019-04-27 06:04:45 -0400

    othello minimax

---
+++ algorithms/othello_minimax.py
@@ -0,0 +1,15 @@
+'''
+othello implementation for minimax
+functions:
+    minimax - recursive
+    empty board - if the board is empty or not
+    evaluate - max value at any point
+
+'''
+
+board = [[],[],[]]
+def minimax(board):
+    return 5
+
+def isEmpty(board):
+    return False

98716885400a6d6535bec439b98ca418822d4952
aryan <aryansingh@umass.edu>
2019-08-25 21:22:09 -0700

    print binary

---
+++ extra-practice-problems/printBinary.py
@@ -0,0 +1,12 @@
+# binary table
+# truth table 
+
+def printBinary(digits, prefix):
+    if digits == 0:
+        print(prefix)
+
+    else:
+        printBinary(digits-1, prefix + "0")
+        printBinary(digits-1, prefix+"1")
+
+printBinary(3, "")

9e5f6abdd0c2a1a605c86174c7ccec7823b2a835
aryan <aryansingh@umass.edu>
2019-08-26 22:54:41 -0700

    largest_subarray unique

--- extra-practice-problems/largest_subarray.py
+++ extra-practice-problems/largest_subarray.py
@@ -1,14 +1,22 @@
 # if the elements of the array are unique
-def largest_subarray(a, k):
-  # store first starting index for a subarray as the largest, since 'a' will always be <= 'k'
+def largest_subarray_unique(a, k):
   first_idx = 0
-  # check indices where a subarray of size k can be made
   for x in range(1, len(a) - k + 1):
-    # replace the largest first index if a larger value is found
     if a[first_idx] < a[x]:
       first_idx = x
-
-  # return subarray starting at that largest possible first index with size k
   return a[first_idx:first_idx+k]
 
-print(largest_subarray([1,4,3,2,5],4))
+print(largest_subarray_unique([1,4,3,2,5],4))
+
+def largest_subarray(a,k):
+  first_idx = 0
+  for x in range(1, len(a) - k + 1):
+    for i in range(k):
+      if a[first_idx] <= a[x]:
+        first_idx = x
+        break
+      else:
+        break
+  return a[first_idx:first_idx+k]
+
+print(largest_subarray([1,4,3,3,6,2,5,6],4))

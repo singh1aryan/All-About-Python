f5ab5f75bbdc1d9cb0b75eddb7109a0d35ef3c4c
aryan <aryansingh@umass.edu>
2019-09-03 18:11:53 -0400

    word-search

---
+++ extra-practice-problems/necklace_beads.py
@@ -0,0 +1,3 @@
+# Necklace beads question asked in an online interview
+# You have 2 arrays of same length, each index in the first corresponds to the beads in second
+# They are all connected in a sense
--- leetcode/79.word-search.py
+++ leetcode/79.word-search.py
@@ -5,15 +5,50 @@
         vertical, horizontal
         return true if there
         
+    [
+        ['a','b','a'],
+        ['c','d','e'],
+        ['a','g','f'],
+    ]
 Strategy:
     dfs - 
 '''
 
-def word_search(m,n):
-    for i in range(0,m):
-        for j in range(0,n):
-            if exist(i,j,) == True:
+def word_search(board, word):
+    for i in range(0,len(board)):
+        for j in range(0,len(board[0])):
+            if board[i][j]==word[0] and exist(board, word, i,j, 0):
                 return True
+
+    return False
             
-def exist():
+# DFS algorithm, going deep in the matrix to find the word
+def exist(board, word, i, j, k):
+
+    if i<0 or j<0 or i>=len(board) or j>=len(board[0]) or k>=len(word) or word[k]!=board[i][j]:
+        # print(1)
+        return False
     
+    ch = word[k]
+    # Does this have to come?
+    # Where to print the true statement for recursive functions?
+    if ch == word[len(word)-1] and k==len(word)-1:
+        return True
+    # backtracking - choose, explore and unchoose
+    board[i][j] = '-'
+    if(exist(board, word, i+1, j, k+1) or exist(board, word, i, j+1, k+1) or exist(board, word, i-1, j, k+1) or exist(board, word, i, j-1, k+1)):
+        return True
+    board[i][j] = ch
+
+
+a = [
+    ['a','b','a'],
+    ['c','d','e'],
+    ['a','g','f'],]
+print(word_search(a, "abdg"))# true
+print(word_search(a, "abdgf"))# true
+print(word_search(a, "abd"))# true
+print(word_search(a, "abdc"))# true
+
+
+# Learning points - Recursion, DFS, Backtracking + DFS, How to recursively backtrack?

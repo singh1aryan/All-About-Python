feebb5e1cd5bd9a79375a04ed835911d6f03d323
aryan <aryansingh@umass.edu>
2019-09-04 22:32:13 -0400

    max-product-3

--- extra-practice-problems/maxProductThree.py
+++ extra-practice-problems/maxProductThree.py
@@ -1,11 +1,20 @@
 # [-4, -4, 2, 8] should return 128 as the largest product can be made by multiplying -4 * -4 * 8 = 128.
 
-# Here's a starting point:
-
-def maximum_product_of_three(nums):
+# works for only all positive array
+def maximum_product(nums):
     nums = sorted(nums)
     return nums[-1]*nums[-2]*nums[-3]
   # Fill this in.
 
+print(maximum_product([4, 4, 2, 8]))
+# 128
+
+
+def maximum_product_of_three(nums):
+    # looks dp on the first go, keep track of sum of 3
+    nums = sorted(nums)
+    return max(nums[0]*nums[1]*nums[-1], nums[-1]*nums[-2]*nums[-3])
+
 print(maximum_product_of_three([-4, -4, 2, 8]))
 # 128
+

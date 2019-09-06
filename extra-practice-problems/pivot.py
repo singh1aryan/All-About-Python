3142fcf75a223b2dedb226530a4b126943307f0b
aryan <aryansingh@umass.edu>
2019-09-06 09:27:01 -0400

    pivot

---
+++ extra-practice-problems/pivot.py
@@ -0,0 +1,29 @@
+# Example 1:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+
+
+def pivot(nums):
+    if len(nums)==0:
+        return -1
+    if len(nums)==0:
+        return nums[0]
+    start =0
+    end = len(nums)-1
+    # kind of like binary search but finding the smallest element 
+    while start <= end:
+        mid = (start+end)//2
+        if nums[mid]>nums[mid+1]:
+            return mid+1
+        elif nums[start]<= nums[mid]:
+            start = mid+1
+        else:
+            end = mid-1
+    return 0
+
+print(pivot([4,5,6,7,0,1,2])) #prints 4 - index for the pivot

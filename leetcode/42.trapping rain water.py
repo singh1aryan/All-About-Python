12f716c618026d6802869513812beaa9218403b3
aryansingh12 <jnkenney@umass.edu>
2019-09-09 13:43:39 -0400

    trapping rain

---
+++ leetcode/42.trapping rain water.py
@@ -0,0 +1,20 @@
+def trap(self, height):
+        left = 0
+        right = len(height)-1
+        ans = 0
+        left_max = 0
+        right_max = 0
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    ans += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    ans += right_max - height[right]
+                right -= 1
+        return ans

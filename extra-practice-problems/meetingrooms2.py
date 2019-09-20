01d392458f1d8099348e26e2801c67c7255d25cf
Aryan Singh <aryansingh@umass.edu>
2019-09-20 19:50:19 -0400

    test case

--- extra-practice-problems/meetingrooms2.py
+++ extra-practice-problems/meetingrooms2.py
@@ -2,15 +2,13 @@ def meeting(start, ends):
     all = [(s,1) for s in start] + [(s,-1) for s in ends]
 
     all = sorted(all)
-    print(all)
     num=0
     largest=0
+    print(all)
     for pos, t in all:
         num += t
         if largest < num:
             largest = num
-            print(largest)
-    
     return largest
 
-meeting([1,2,6,5,3],[5,5,7,6,8])
+print(meeting([1,1,2],[1,2,2]))

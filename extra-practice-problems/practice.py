b31b68310605e5026aec33d26ac66896dd66c8a0
Aryan Singh <aryansingh@umass.edu>
2019-10-04 12:46:42 -0400

    practice

--- extra-practice-problems/practice.py
+++ extra-practice-problems/practice.py
@@ -8,7 +8,7 @@ def quicksort(nums):
     return quicksort(left)+middle+quicksort(right)
 
 
-print(quicksort([1,3,4,2,6,5]))
+# print(quicksort([1,3,4,2,6,5]))
 
 """
 understand the base case, don't just write anything
@@ -18,3 +18,37 @@ then we combine back to form the array again
 quicksort - we make left,right and middle arrays and then combine them
 we do this recursively but in a sense that we return an array at the end
 """
+
+# b = {0:1,1:3}
+# print(b[::-1])
+# a= []
+# for i in range(1000000):
+#     # for j in range(1000000):
+#     a.append([i])
+# print(a[0])
+
+# subsets with this, woah
+a=[]
+def rec(soFar, rest):
+    if len(rest)==0:
+        a.append(soFar)
+    else:
+        rec(soFar + [rest[0]], rest[1:])#inclusion
+        rec(soFar, rest[1:])#exclusion
+# rec([],[1,2,3])
+# print(a)
+import sys
+sys.setrecursionlimit(50)
+per = []
+def permute(a,b):
+    if len(a)==3:
+        print(a)
+        per.append(a)
+    else:
+        for i in range(len(b)):
+            r = b[0:i]+b[i+1:]#exclusion
+            print(r)
+            permute(a+[b[i]], r)#inclusion
+
+permute([],[1,2,3])
+print(per)
